4facffc19885c51194a832d8fd6fc7641d1179fe
ojambo <ojambo04@gmail.com>
2019-07-31 17:44:04 +0530

    Attendance data upload

--- src/app/attendance/components/admin/upload-data/upload-data.component.ts
+++ src/app/attendance/components/admin/upload-data/upload-data.component.ts
@@ -1,23 +1,49 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-import { FileUploader } from 'ng2-file-upload';
+import { baseURL } from "src/app/shared/config/baseUrl";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { FileUploader } from "ng2-file-upload";
+import { AuthService } from "src/app/auth/services/auth.service";
 
 @Component({
-  selector: 'app-upload-data',
-  templateUrl: './upload-data.component.html',
-  styleUrls: ['./upload-data.component.scss']
+  selector: "app-upload-data",
+  templateUrl: "./upload-data.component.html",
+  styleUrls: ["./upload-data.component.scss"]
 })
 export class UploadDataComponent implements OnInit {
   // training order file upload
   // selectedFile: File = null;
   // progressValue = 0;
-  public uploader:FileUploader = new FileUploader({url: ''});
+  public uploader: FileUploader;
 
-  constructor(private locationService: Location,
-    private snackbar: MatSnackBar) { }
+  constructor(
+    private locationService: Location,
+    private snackbar: MatSnackBar,
+    private auth: AuthService
+  ) {}
 
   ngOnInit() {
+    const projectId = this.auth.currentUser.project;
+    const url =
+      baseURL + `api/attendance/project/${projectId}/attendance-data/upload`;
+    const token = 'Bearer ' + this.auth.getToken().split('"').join('')
+
+    this.uploader = new FileUploader({
+      url: url,
+      itemAlias: "dataFile",
+      authTokenHeader:  'Authorization',
+      authToken: token
+    });
+
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (
+      item: any,
+      response: any,
+      status: any,
+      headers: any
+    ) => {
+      console.log("ImageUpload:uploaded:", item, status, response);
+    };
   }
 
   // selectFile(event) {
@@ -45,7 +71,8 @@ export class UploadDataComponent implements OnInit {
   // }
 
   goBack() {
-    this.locationService.back()
+    this.locationService.back();
   }
 
+  insertToPuchingRec() {}
 }
