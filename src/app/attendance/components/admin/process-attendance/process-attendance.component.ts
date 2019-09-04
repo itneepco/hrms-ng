200a725be326bb351873aa1f1e905b06c6c32b96
ojambo <ojambo04@gmail.com>
2019-09-04 12:01:52 +0530

    Disable process attendance for today

--- src/app/attendance/components/admin/process-attendance/process-attendance.component.ts
+++ src/app/attendance/components/admin/process-attendance/process-attendance.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { AttendanceFileStatus } from 'src/app/attendance/models/attendance-file-status';
-import { WageMonth } from 'src/app/attendance/models/wage-month';
-import { AttendanceDataService } from 'src/app/attendance/services/attendance-data.service';
-import { WageMonthService } from 'src/app/attendance/services/wage-month.service';
-import { DateService } from 'src/app/shared/services/date.service';
+import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { AttendanceFileStatus } from "src/app/attendance/models/attendance-file-status";
+import { WageMonth } from "src/app/attendance/models/wage-month";
+import { AttendanceDataService } from "src/app/attendance/services/attendance-data.service";
+import { WageMonthService } from "src/app/attendance/services/wage-month.service";
+import { DateService } from "src/app/shared/services/date.service";
 
 @Component({
-  selector: 'app-process-attendance',
-  templateUrl: './process-attendance.component.html',
-  styleUrls: ['./process-attendance.component.scss']
+  selector: "app-process-attendance",
+  templateUrl: "./process-attendance.component.html",
+  styleUrls: ["./process-attendance.component.scss"]
 })
 export class ProcessAttendanceComponent implements OnInit {
   activeWageMonth: WageMonth;
@@ -18,39 +18,52 @@ export class ProcessAttendanceComponent implements OnInit {
   dates: Date[];
   biometricFileStatus: AttendanceFileStatus[];
 
-  constructor(private wageMonthService: WageMonthService,
+  constructor(
+    private wageMonthService: WageMonthService,
     private snackbar: MatSnackBar,
     private dateService: DateService,
-    private attendanceDataService: AttendanceDataService) { }
+    private attendanceDataService: AttendanceDataService
+  ) {}
 
   ngOnInit() {
     this.wageMonthService.getActiveWageMonth().subscribe(wageMonth => {
-      if (!wageMonth) return
+      if (!wageMonth) return;
 
-      this.activeWageMonth = wageMonth
-      this.startDate = this.activeWageMonth.from_date
-      this.endDate = this.dateService.increaseDateByMonth(this.activeWageMonth.to_date, 1)
+      this.activeWageMonth = wageMonth;
+      this.startDate = this.activeWageMonth.from_date;
+      this.endDate = this.dateService.increaseDateByMonth(
+        this.activeWageMonth.to_date,
+        1
+      );
 
-      this.attendanceDataService.getFileUploadedStatus(this.startDate, this.endDate)
+      this.attendanceDataService
+        .getFileUploadedStatus(this.startDate, this.endDate)
         .subscribe(result => {
           // console.log(result)
-          this.biometricFileStatus = result
-        })
-    })
+          this.biometricFileStatus = result;
+        });
+    });
   }
 
   processData(status: AttendanceFileStatus) {
-    this.attendanceDataService.processPunchingData(status.punch_day)
-      .subscribe(data => {
-        // console.log(data)
-        this.snackbar.open(
-          data.message,
-          "Dismiss",
-          {
-            duration: 2000
-          }
-        );
-        status.is_processed = true;
-      })
+    if (this.dateService.compareDates(new Date(), status.punch_day)) {
+      this.snackbar.open(
+        "Please process the attendance data one day later",
+        "Dismiss",
+        { duration: 2000 }
+      );
+      return;
+    }
+
+    this.attendanceDataService.processPunchingData(status.punch_day).subscribe(
+      result => {
+        // console.log(result)
+        this.snackbar.open(result.message, "Dismiss", { duration: 2000 });
+        status.is_processed = result.error ? false : true;
+      },
+      err => {
+        status.is_processed = false;
+      }
+    );
   }
 }
